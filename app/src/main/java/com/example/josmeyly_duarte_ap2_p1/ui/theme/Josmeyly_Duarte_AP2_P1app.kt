1b1632939ae07c7aed500665c2807a5b373d7c72
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-02-04 18:24:28 -0400

    se agrego el android name en manifest

---
+++ app/src/main/java/com/example/josmeyly_duarte_ap2_p1/ui/theme/Josmeyly_Duarte_AP2_P1app.kt
@@ -0,0 +1,8 @@
+package com.example.josmeyly_duarte_ap2_p1.ui.theme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class Josmeyly_Duarte_AP2_P1app : Application()
