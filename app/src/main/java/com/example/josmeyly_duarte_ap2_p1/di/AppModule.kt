3628bffdaf0e452f1c78df2026ac056fcc7353dc
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-02-04 19:04:18 -0400

    Se creo en la carpeta di y dentro de esta la clase object de AppModule.kt

---
+++ app/src/main/java/com/example/josmeyly_duarte_ap2_p1/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.josmeyly_duarte_ap2_p1.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
