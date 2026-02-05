55b213cba89cdbb60270e1b77bbcc022b51e5d0b
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-02-04 20:00:43 -0400

    se agrego lo que faltaba al appmodule

--- app/src/main/java/com/example/josmeyly_duarte_ap2_p1/di/AppModule.kt
+++ app/src/main/java/com/example/josmeyly_duarte_ap2_p1/di/AppModule.kt
@@ -1,10 +1,28 @@
 package com.example.josmeyly_duarte_ap2_p1.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.josmeyly_duarte_ap2_p1.data.local.database.BorrameDb
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideEstudianteDb(
+        @ApplicationContext appContext: Context
+    ): BorrameDb = Room.databaseBuilder(
+        appContext,
+        BorrameDb::class.java,
+        "Borrame.db"
+    )
+        .fallbackToDestructiveMigration()
+        .build()
 }
