b89f458f63786dc201d7f2242467095ca99fee58
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2026-02-11 21:18:00 -0400

    el dao de cerveza

--- app/src/main/java/com/example/josmeyly_duarte_ap2_p1/data/local/dao/BorrameDao.kt
+++
@@ -1,7 +0,0 @@
-package com.example.josmeyly_duarte_ap2_p1.data.local.dao
-
-import com.example.josmeyly_duarte_ap2_p1.data.local.entities.BorrameEntity
-
-interface BorrameDao {
-    suspend fun upsert(asignatura: BorrameEntity)
-}
---
+++ app/src/main/java/com/example/josmeyly_duarte_ap2_p1/data/local/dao/CervezaDao.kt
@@ -0,0 +1,33 @@
+package com.example.josmeyly_duarte_ap2_p1.data.local.dao
+
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.josmeyly_duarte_ap2_p1.data.local.entities.CervezaEntity
+import kotlinx.coroutines.flow.Flow
+
+interface CervezaDao {
+    @Upsert
+    suspend fun upsert(cerveza: CervezaEntity)
+
+    @Query("DELETE FROM Cervezas WHERE cervezaId=:id")
+    suspend fun delete(id:Int)
+
+    @Query("SELECT *FROM Cervezas WHERE cervezaId=:id")
+    suspend fun getById(id:Int): CervezaEntity?
+
+    @Query("SELECT EXISTS(SELECT 1 FROM Cervezas WHERE LOWER(nombre)=LOWER(:nombre)AND cervezaId !=:excludeId)")
+    suspend fun existsByNombre(nombre:String,excludeId:Int): Boolean
+
+    @Query("SELECT * FROM Cervezas WHERE cervezaId=:id")
+    fun observeById(id:Int): Flow<CervezaEntity>
+
+    @Query("SELECT * FROM Cervezas ORDER BY nombre ASC ")
+    fun observeAll(id:Int):Flow<List<CervezaEntity>>
+
+
+
+}
+
+
+
+
